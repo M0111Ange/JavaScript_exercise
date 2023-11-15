7cfb4b6197280970d7e030637a125ec49d45b3c8
M0111Ange <angemukundente@gmail.com>
2023-11-15 16:38:20 +0200

    updated repo

---
+++ check_odd.js
@@ -0,0 +1,9 @@
+//Write a function that checks if a number is odd.
+function checkOdd(number){
+    if(number%2 !==0){
+        return "odd";
+    }else{
+        return "not odd";
+    }
+}
+console.log(checkOdd(12));
---
+++ max_nbr.js
@@ -0,0 +1,11 @@
+//Write a function that returns the maximum of two numbers.
+
+
+function maxOfTwoNumbers(a, b) {
+    return Math.max(a, b);
+}
+
+var num1 = 10;
+var num2 = 100;
+
+console.log(maxOfTwoNumbers(num1,num2));
